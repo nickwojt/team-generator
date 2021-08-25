d801ef89a8ccdfe5818a24783e6e2cb865306d2b
Nicholas Wojtalewicz <nicholaswojt@outlook.com>
2021-08-24 21:14:12 -0600

    Created proper functions inside index.js and finished inquirer prompts

--- index.js
+++ index.js
@@ -4,18 +4,241 @@ const Manager = require("./lib/manager");
 const Engineer = require("./lib/engineer");
 const inquirer = require("inquirer");
 
-inquirer.prompt([
-  {
-    type: "input",
-    message: "Please create your team. What is your managers name?",
-    name: "managerName",
-  },
-  {
-    type: "input",
-    message: "Enter the managers ID",
-    name: "managerId",
-  },
-  {
-    type: "input",
-  },
-]);
+const employees = [];
+
+init();
+
+function init() {
+  renderManager();
+}
+
+function renderManager() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is your name?",
+        name: "name",
+        validate: (answer) => {
+          if (answer !== "") {
+            return true;
+          }
+          return "Please enter at least one character.";
+        },
+      },
+      {
+        type: "input",
+        message: "Enter an employee ID",
+        name: "id",
+        validate: (answer) => {
+          if (answer !== "") {
+            return true;
+          }
+          return "Please enter at least one character.";
+        },
+      },
+      {
+        type: "input",
+        message: "Enter an email",
+        name: "email",
+        validate: (answer) => {
+          const pass = answer.match(/\S+@\S+\.\S+/);
+          if (pass) {
+            return true;
+          }
+          return "Please enter a valid email address.";
+        },
+      },
+      {
+        type: "input",
+        message: "What is your office number?",
+        name: "office",
+        validate: (answer) => {
+          if (answer !== "") {
+            return true;
+          }
+          return "Please enter at least one character.";
+        },
+      },
+    ])
+    .then((response) => {
+      const manager = new Manager(
+        response.name,
+        response.id,
+        response.email,
+        response.office
+      );
+      employees.push(manager);
+      anotherMember();
+    });
+}
+
+function renderEngineer() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is your name?",
+        name: "name",
+        validate: (answer) => {
+          if (answer !== "") {
+            return true;
+          }
+          return "Please enter at least one character.";
+        },
+      },
+      {
+        type: "input",
+        message: "Enter an employee ID",
+        name: "id",
+        validate: (answer) => {
+          if (answer !== "") {
+            return true;
+          }
+          return "Please enter at least one character.";
+        },
+      },
+      {
+        type: "input",
+        message: "Enter an email",
+        name: "email",
+        validate: (answer) => {
+          const pass = answer.match(/\S+@\S+\.\S+/);
+          if (pass) {
+            return true;
+          }
+          return "Please enter a valid email address.";
+        },
+      },
+      {
+        type: "input",
+        message: "What is your Github username?",
+        name: "github",
+        validate: (answer) => {
+          if (answer !== "") {
+            return true;
+          }
+          return "Please enter at least one character.";
+        },
+      },
+    ])
+    .then((response) => {
+      const engineer = new Engineer(
+        response.name,
+        response.id,
+        response.email,
+        response.github
+      );
+      employees.push(engineer);
+      anotherMember();
+    });
+}
+
+function renderIntern() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is your name?",
+        name: "name",
+        validate: (answer) => {
+          if (answer !== "") {
+            return true;
+          }
+          return "Please enter at least one character.";
+        },
+      },
+      {
+        type: "input",
+        message: "Enter an employee ID",
+        name: "id",
+        validate: (answer) => {
+          if (answer !== "") {
+            return true;
+          }
+          return "Please enter at least one character.";
+        },
+      },
+      {
+        type: "input",
+        message: "Enter an email",
+        name: "email",
+        validate: (answer) => {
+          const pass = answer.match(/\S+@\S+\.\S+/);
+          if (pass) {
+            return true;
+          }
+          return "Please enter a valid email address.";
+        },
+      },
+      {
+        type: "input",
+        message: "What school did you attend?",
+        name: "school",
+        validate: (answer) => {
+          if (answer !== "") {
+            return true;
+          }
+          return "Please enter at least one character.";
+        },
+      },
+    ])
+    .then((response) => {
+      const intern = new Intern(
+        response.name,
+        response.id,
+        response.email,
+        response.school
+      );
+      employees.push(intern);
+      anotherMember();
+    });
+}
+
+function anotherMember() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        message: "Would you like to add another team member?",
+        choices: ["Engineer", "Intern", "None"],
+        name: "type",
+      },
+    ])
+    .then((response) => {
+      switch (response.type) {
+        case "Engineer":
+          renderEngineer();
+          break;
+        case "Intern":
+          renderIntern();
+          break;
+        default:
+          generateHTML();
+          break;
+      }
+    });
+}
+
+function generateHTML() {
+  console.log(employees);
+  const filterManager = employees.filter((em) => em.getRole() === "Manager");
+
+  const filterIntern = employees.filter((em) => em.getRole() === "Intern");
+
+  const filterEngineer = employees.filter((em) => em.getRole() === "Engineer");
+  console.log(filterEngineer);
+  console.log(filterIntern);
+  console.log(filterManager);
+  const htmlManager = filterManager.map((item) => {
+    return `<div>`;
+  });
+
+  const htmlIntern = filterIntern.map((item) => {
+    return ``;
+  });
+
+  const htmlEngineer = filterEngineer.map((item) => {
+    return ``;
+  });
+}
